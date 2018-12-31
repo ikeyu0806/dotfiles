c562fd7d7f36efd3fb72762d5be18bb22fc7b544
ikeyu0806 <ikeyu0806@gmail.com>
2018-12-31 21:32:13 +0900

    git設定追加

--- link.sh
+++ link.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # ln -sf ~/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/dotfiles/.vimrc ~/.vimrc
-# ln -sf ./.gitconfig ~/.gitconfig
-# ln -sf ./.gitignore ~/.gitignore
+ln -sf ~/dotfiles/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/dotfiles/.gitignore ~/.gitignore
