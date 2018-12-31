8e52bcf3578ea5b87542d22b59249b763d5ee70b
ikeyu0806 <ikeyu0806@gmail.com>
2018-12-31 15:59:44 +0900

    自動化は一旦保留

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,7 @@
+# そのうち作る
+
 #!/bin/bash
 # ln -sf ~/.bash_profile ~/.bash_profile
-ln -sf ~/.vimrc
+# ln -sf ./.vimrc ~/.vimrc
 # ln -sf ./.gitconfig ~/.gitconfig
 # ln -sf ./.gitignore ~/.gitignore
