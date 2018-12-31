6a9a35817b0715dc15982cd0aab26d2ad7ac0d4a
ikeyu0806 <ikeyu0806@gmail.com>
2018-12-31 21:16:12 +0900

    シンボリックリンクのパス修正

--- link.sh
+++ link.sh
@@ -1,7 +1,5 @@
-# そのうち作る
-
 #!/bin/bash
 # ln -sf ~/.bash_profile ~/.bash_profile
-# ln -sf ./.vimrc ~/.vimrc
+ln -sf ~/dotfiles/dotfiles/.vimrc ~/.vimrc
 # ln -sf ./.gitconfig ~/.gitconfig
 # ln -sf ./.gitignore ~/.gitignore
