52160b7f2d090c1fbb158e339c0b17e7c44ed2a5
ikeyu0806 <ikeyu0806@gmail.com>
2019-01-03 19:09:20 +0900

    bash_profile追加

--- link.sh
+++ link.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+ln -sf ~/dotfiles/dotfiles/.bashprofile ~/.bash_profile
 ln -sf ~/dotfiles/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/dotfiles/.gitignore ~/.gitignore
-ln -sf ~/dotfiles/dotfiles/.aliases ~/.aliases
-# source ~/.bashrc
