a6c1baaa44dfccf3c5e1fdbf2895a5232c231740
ikeyu0806 <ikeyu0806@gmail.com>
2019-01-08 12:58:23 +0900

    gitignore変更

--- link.sh
+++ link.sh
@@ -3,4 +3,4 @@ ln -sf ~/dotfiles/dotfiles/.bashprofile ~/.bash_profile
 ln -sf ~/dotfiles/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/dotfiles/.gitignore ~/.gitignore
+ln -sf ~/dotfiles/dotfiles/.gitignoreglobal ~/.gitignore_global
