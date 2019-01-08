e217d3952dc107f88f8c821cbf2c5b5745fcc566
ikeyu0806 <ikeyu0806@gmail.com>
2019-01-08 17:49:19 +0900

    bash_profileの表記修正

--- link.sh
+++ link.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ln -sf ~/dotfiles/dotfiles/.bashprofile ~/.bash_profile
+ln -sf ~/dotfiles/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/dotfiles/.gitconfig ~/.gitconfig
