647f4b73e2a9be551480fb155a77adb3fa9836cd
ikeyu0806 <ikeyu0806@gmail.com>
2019-09-08 11:24:13 +0900

    modify link.sh

--- link.sh
+++ link.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-ln -sf ~/dotfiles/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/dotfiles/.gitignore_global ~/.gitignore_global
-ln -sf ~/dotfiles/dotfiles/.tmux.conf ~/.tmux.conf
+DOT_DIRECTORY="${HOME}/dotfiles/dotfiles"
 
-for extension in `cat ~/dotfiles/vscode/vscode_extensions.txt`; do
-    code --install-extension $extension
+echo "link home directory dotfiles"
+
+for f in .??*
+do
+  ln -sf ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 done
 
+for extension in `cat ~/dotfiles/vscode/vscode_extensions.txt`; do
+  code --install-extension $extension
+done
 
-cat << EOS
-設定ファイルを置き換えました！
-.bash_profile
-.bashrc
-.vimrc
-.gitconfig
-.gitignore_global
-VSCODEの拡張機能をインストールしました！
-EOS
+echo "linked dotfiles complete"
